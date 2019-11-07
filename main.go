cefdf7890f1dc7ca0ffea110faf373bc156da880
lcc142625 <1210233414@qq.com>
2019-11-07 15:49:17 +0800

    div.go

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"gitTest/calc"
 )
 func main() {
+	fmt.Println("calc.Add called!")
 	res := calc.Add(10, 20)
 	fmt.Println("Add(10 ,20) :", res)
+
+	fmt.Println("calc.Sub called!")
+	res = calc.Sub(30, 20)
+	fmt.Println("Sub(30 ,20) :", res) //fmt
+
+	fmt.Println("calc.Multi called!")
+	res = calc.Multi(30, 20)
+	fmt.Println("Multi(30 ,20) :", res)
+
+	fmt.Println("calc.Div called!")
+	res = calc.Div(100, 20)
+	fmt.Println("Div(100 ,20) :", res)
 }
